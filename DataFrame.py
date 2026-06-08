95e759dabd105b976e32901ca2bd7b7d50f84511
Chinthalapalliharinathreddy <chinthalapalli.harinathreddy@gmail.com>
2026-06-08 17:05:22 +0530

    I have implemented a Pandas Series and DataFrame Topics with methods and coding

---
+++ DataFrame.py
@@ -0,0 +1,30 @@
+#Creating a dataframe
+import pandas as pd
+data={
+    "Name": ["Hari", "Ram", "Alex"],
+    "Marks": [85, 90, 95],
+    "Grade": ["A", "A+", "A+"]
+} 
+df=pd.DataFrame(data)
+print(df)
+
+#Dataframe attributes
+print(df.columns)
+print(df.shape)
+print(df.dtypes)
+
+#dataframe attributes
+import pandas as pd
+df = pd.DataFrame({
+    "Name": ["Hari", "Ram"],
+    "Age": [21, 22]
+})
+
+print("Shape:", df.shape)
+print("Columns:", df.columns)
+print("Dimensions:", df.ndim)
+print("Size:", df.size)
+print("Types:\n", df.dtypes)
+
+
+#dataframe methods()
